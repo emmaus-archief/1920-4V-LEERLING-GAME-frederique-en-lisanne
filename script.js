572fefd92178e2ea01116220421bc071dad4302e
FrederiqueK <frederiquekeijzer@gmail.com>
2020-05-19 12:57:35 +0000

    verbetering - spelerSpringen en zwaartekracht

--- script.js
+++ script.js
@@ -26,12 +26,17 @@ var canvasBreedte =  1280;
 var canvasHoogte = 720;
 var veldBreedte = 1240;
 var veldHoogte = 680;
-var grasHoogte = 600;
+var grasHoogte = 650;
 
 var spelerBreedte = 50; 
 var spelerHoogte = 50;
 var spelerX = 50; // x-positie van speler
 var spelerY = grasHoogte - spelerHoogte; // y-positie van speler
+
+var maxSnelheid = 10;
+var nieuweSnelheid = 0;
+var spelerVersnelling = 0.25;
+var spelerSnelheid = 0.25;
 
 var kogelX = 0;    // x-positie van kogel
 var kogelY = 0;    // y-positie van kogel
@@ -45,12 +50,12 @@ var score = 0; // aantal behaalde punten
 var jump = false; // moet er gesprongen worden?
 var richting = 1; //de zwaartekracht in y richting
 var versnelling = 8; //snelheid van speler
-var springKracht = 17; //hoe hoog de speler kan springen
-var valSnelheid = 8; //gelijk aan versnelling
-var minHeight = grasHoogte - spelerHoogte; //height van de grond
-var maxHeight = 55;  //height van de lucht
+var springKracht = 10; //hoe hoog de speler kan springen
+var valSnelheid = 1; 
+var minHoogte = grasHoogte - spelerHoogte; //hoogte van de grond
+var maxHoogte = 20; 
 var sprongTeller = 0; //houdt bij hoevaak de speler springt
-
+var springRichting = 0;
 
 /* ********************************************* */
 /*      functies die je gebruikt in je game      */
@@ -67,9 +72,10 @@ var tekenVeld = function () {
   //gras (ondergrond)
   noStroke();
   fill(100, 200, 75); // groen
-  rect(20, grasHoogte, veldBreedte, 100);
+  rect(20, grasHoogte, veldBreedte, 50);
 
 };
+
 
 /**
  * Tekent de vijand
@@ -124,24 +130,44 @@ var beweegKogel = function() {
  * Kijkt wat de toetsen/muis etc zijn.
  * Updatet globale variabele spelerX en spelerY
  */
+
 var beweegSpeler = function() {
-  if (keyIsPressed && keyCode === 65) { // links
-    spelerX = spelerX - 5;
-  } else if (keyIsPressed && keyCode === 68) { // rechts
-    spelerX = spelerX + 5; }
+ if (keyIsPressed && keyCode === 65) { // "a" links
+    nieuweSnelheid = spelerSnelheid + spelerVersnelling; // de snelheid van de speler neemt toe 
+    spelerSnelheid = nieuweSnelheid;
+    spelerX = spelerX - nieuweSnelheid;
+    springRichting = -nieuweSnelheid;
+  }
+  
+  else if (keyIsPressed && keyCode === 68) { // "d" rechts
+    nieuweSnelheid = nieuweSnelheid + spelerVersnelling; // de snelheid van de speler neemt toe 
+    spelerSnelheid = nieuweSnelheid;
+    spelerX = spelerX + nieuweSnelheid;
+    springRichting = nieuweSnelheid;
+  }
+  
+  else {
+    spelerSnelheid = 0.25; // de snelheid van de speler wordt weer normaal als de speler heeft stilgestaan
+    nieuweSnelheid = 0;
+  } 
+
+  if (spelerSnelheid >= maxSnelheid) { //de speler kan niet sneller dan de max snelheid
+    spelerSnelheid = maxSnelheid;
+  }
 
   if (spelerX < 20)  { 
-    spelerX = spelerX + 5; //speler kan niet links van het speelveld
+    spelerX = spelerX + 10; //speler kan niet links van het speelveld
   }
-  if (spelerX > veldBreedte - spelerBreedte ) {
-    spelerX = spelerX - 5; //speler kan niet rechts van het speelveld
-  }
+
+  if (spelerX >= veldBreedte - spelerBreedte/2) {
+    spelerX = spelerX - 10; //speler kan niet rechts van het speelveld
+  } 
 };
 
 function spelerSpringen() {
-  if (keyCode === 32) { // spatie
+  if (keyIsPressed && keyCode === 32) { // spatie
     jump = true; //springen
-  } 
+  }
   else {
     jump = false; //niet springen
   }
@@ -178,29 +204,32 @@ var checkGameOver = function() {
   return false;
 };
 
-
 //zwaartekracht
 function zwaartekracht() {
-  if (spelerY >= minHeight && jump === false) {
+  if (spelerY >= minHoogte && jump === false) {
     spelerY = spelerY; //niet meer vallen
     sprongTeller = 0; //reset sprongTeller wanneer speler landt
-  } else {
-    spelerY = spelerY + (richting*versnelling); //speler kan springen
+  } else { // speler kan springen
+    spelerY = spelerY + (richting*versnelling);
+    spelerX = spelerX + springRichting;
   }
 
-  if (jump === true) {
-    if (spelerY <= maxHeight || sprongTeller >= springKracht) {
-      if(spelerY >= minHeight) {
-        spelerY = minHeight; // speler gaat niet lager dan de minHeight (grond) als spatie wordt ingedrukt
-      } else {
-        versnelling = valSnelheid; // vallen als de maximale hoogte is bereikt bij het springen
-      }
+  if (jump === true && spelerY <= maxHoogte || sprongTeller /*+ springSnelheid*/ >= springKracht /*+ springSnelheid*/) {
+    if(spelerY >= minHoogte) {
+      spelerY = minHoogte; // speler gaat niet lager dan de minHoogte (grond) als spatie wordt ingedrukt
+      springRichting = 0; // de volgende keer als de speler springt, springt de speler niet te hoog
     } else {
-      versnelling = -springKracht; // springen
+      vallen(); }
+  } else {
+      versnelling = -springKracht - abs(springRichting) / 2; // springen
       sprongTeller = sprongTeller + 1; //wordt toegevoegd bij sprongTeller
-    } } else {
-      versnelling = valSnelheid;
-  }
+    }
+};
+
+
+function vallen () {;
+  versnelling += valSnelheid; // vallen als de maximale hoogte is bereikt bij het springen
+  
 };
 
 
@@ -212,8 +241,8 @@ function zwaartekracht() {
 function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(canvasBreedte, canvasHoogte);
-
   background('black');
+  alert("(Voor Lisanne) A = links, D = rechts, spatie = springen");
 }
 
 
