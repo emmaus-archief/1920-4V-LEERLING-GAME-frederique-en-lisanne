2ffdfe3dfaae219c02868a770eef80ecaaaadbb6
lisannee <lisanne.boer2004@gmail.com>
2020-05-27 07:23:52 +0000

    anders kan ik niet naar de master

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ var vijandY = 0;   // y-positie van vijand
 var score = 0; // aantal behaalde punten
 var bgImg;
 var img;
-var welkScherm = 0;
+var mode; // geeft weer of de game begonnen is of niet
 
 
 
@@ -47,205 +47,35 @@ var welkScherm = 0;
 /**
  * Tekent het speelveld
  */
+
 var tekenVeld = function () {
   rect(20, 20, width - 2 * 20, height - 2 * 20);
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-}
-
-function startScherm() {
-    background(0);
-    textSize(32);
-    fill(red);
-    text('Klik hier om te starten', 50,20,20,20);
-}
-
-function gameScherm() {
-    fill(blue);
-}
-
-function gameOverScherm() {
-    fill(255,255,255);
-}
+    mode=0; // game nog niet gestart
+    createCanvas(windowWidth,windowHeight);
+};
 
 function draw() {
-    if (welkScherm === 0) {
-        startScherm();
-    } else if (welkScherm === 1) {
-        gameScherm();
-    } else if (welkScherm === 2) {
-        gameOverScherm();
+    clear();
+    if(mode==0) {
+       text();
     }
-}
-
-function startGame() {
-    welkScherm = 1;
-}
-
-function mousePressed() {
-    if(welkScherm === 0) {
-        startGame();
+    if(mode==1) {
+        ellipse(100,100,100,100);
     }
-}
-
-/**
- * Tekent de vijand
- * @param {number} x x-coördinaat
- * @param {number} y y-coördinaat
- */
-var tekenVijand = function(x, y) {
-    
-
 };
 
-
-/**
- * Tekent de kogel of de bal
- * @param {number} x x-coördinaat
- * @param {number} y y-coördinaat
- */
-var tekenKogel = function(x, y) {
-
-
+function keyPressed(){
+    if(keyCode===ENTER) {
+        mode=1;
+    }
 };
 
-function preload() {
-    img = loadImage('img/heidi1.jpg');
-}
-/**
- * Tekent de speler
- * @param {number} x x-coördinaat
- * @param {number} y y-coördinaat
- */
-var tekenSpeler = function(x, y) {
-
-  // Top-left corner of the img is at (0, 0)
-  // Width and height are the img's original width and height
-  image(img, x, y);
-}
-
-
-
-/**
- * Updatet globale variabelen met positie van vijand of tegenspeler
- */
-var beweegVijand = function() {
-    
+var text = function(){
+    fill(0);
+    textSize(20);
+    text('klik hier',20,20);
 };
-
-
-/**
- * Updatet globale variabelen met positie van kogel of bal
- */
-var beweegKogel = function() {
-
-};
-
-
-/**
- * Kijkt wat de toetsen/muis etc zijn.
- * Updatet globale variabele spelerX en spelerY
- */
-var beweegSpeler = function() {
-  if (keyIsPressed && keyCode === 65) { // "a" links
-    spelerX = spelerX - 5;
-  } else if (keyIsPressed && keyCode === 68) { // "d" rechts
-    spelerX = spelerX + 5; 
-  } else if (keyIsPressed && keyCode === 87) { // "w" omhoog
-    spelerY = spelerY - 5; 
-  } else if (keyIsPressed && keyCode === 83) { // "s" omlaag
-    spelerY = spelerY + 5; 
-  }
-};
-
-
-/**
- * Zoekt uit of de vijand is geraakt
- * @returns {boolean} true als vijand is geraakt
- */
-var checkVijandGeraakt = function() {
-
-  return false;
-};
-
-
-/**
- * Zoekt uit of de speler is geraakt
- * bijvoorbeeld door botsing met vijand
- * @returns {boolean} true als speler is geraakt
- */
-var checkSpelerGeraakt = function() {
-    
-  return false;
-};
-
-
-/**
- * Zoekt uit of het spel is afgelopen
- * @returns {boolean} true als het spel is afgelopen
- */
-var checkGameOver = function() {
-    
-  return false;
-};
-
-
-/**
- * setup
- * de code in deze functie wordt één keer uitgevoerd door
- * de p5 library, zodra het spel geladen is in de browser
- */
-
-
-
-
-/*
-function draw() {
-    img(bgImg,0,0);
-}
-function preload () {
-     bgImg = loadImage('img/treeSpring.jpg'); // Load the image
-}
-
-
-/**
- * draw
- * de code in deze functie wordt meerdere keren per seconde
- * uitgevoerd door de p5 library, nadat de setup functie klaar is
- */
-function draw() {
-  switch (spelStatus) {
-    case SPELEN:
-      beweegVijand();
-      beweegKogel();
-      beweegSpeler();
-
-      
-      if (checkVijandGeraakt()) {
-        // punten erbij
-        // nieuwe vijand maken
-      }
-      
-      if (checkSpelerGeraakt()) {
-        // leven eraf of gezondheid verlagen
-        // eventueel: nieuwe speler maken
-      }
-
-      tekenVeld();
-      tekenVijand(vijandX, vijandY);
-      tekenKogel(kogelX, kogelY);
-      tekenSpeler(spelerX, spelerY);
-      startScherm();
-      startGame();
-      gameScherm();
-      gameOverScherm();
-
-      if (checkGameOver()) {
-        spelStatus = GAMEOVER;
-      }
-      break;
-  }
-}
 
