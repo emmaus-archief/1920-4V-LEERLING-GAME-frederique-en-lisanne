bf446b6c1c26bce339fb43f748e9f5c6bf8f87ac
FrederiqueK <frederiquekeijzer@gmail.com>
2020-06-04 12:34:06 +0000

    obstakels en afbeeldingen

--- script.js
+++ script.js
@@ -28,8 +28,8 @@ var veldBreedte = 1240;
 var veldHoogte = 680;
 var grasHoogte = 650;
 
-var spelerBreedte = 50; 
-var spelerHoogte = 75;
+var spelerBreedte = 75; 
+var spelerHoogte = 112.5;
 var spelerX = 50; // x-positie van speler
 var spelerY = grasHoogte - spelerHoogte; // y-positie van speler
 
@@ -38,6 +38,8 @@ var maxSnelheid = 10;
 var nieuweSnelheid = 0;
 var spelerVersnelling = 0.25;
 var spelerSnelheid = 0.25;
+var naarRechts = false;
+var naarLinks = false;
 
 var kogelX = 0;    // x-positie van kogel
 var kogelY = 0;    // y-positie van kogel
@@ -58,6 +60,12 @@ var maxHoogte = 20;
 var sprongTeller = 0; //houdt bij hoevaak de speler springt
 var springRichting = 0;
 
+var spelerImage;
+var ondergrondImg;
+var obstakelImage;
+var achtergrondImg;
+var schaapImg;
+
 /* ********************************************* */
 /*      functies die je gebruikt in je game      */
 /* ********************************************* */
@@ -67,13 +75,12 @@ var springRichting = 0;
  * Tekent het speelveld
  */
 var tekenVeld = function () {
-  fill("blue");
-  rect(20, 20, veldBreedte, veldHoogte);
+  image(achtergrondImg, 20, 20, veldBreedte, veldHoogte);
 
   //gras (ondergrond)
   noStroke();
-  fill(100, 200, 75); // groen
-  rect(20, grasHoogte, veldBreedte, 50);
+  //fill(100, 200, 75); // groen
+  image(ondergrondImg, 20, grasHoogte, veldBreedte, 50);
 
 };
 
@@ -88,25 +95,43 @@ var tekenVijand = function(x, y) {
 
 };
 
+var schaapBreedte = 80;
+var schaapHoogte = 60;
+var schaapIsZichtbaar = true;
 
 /**
- * Tekent de kogel of de bal
- * @param {number} x x-coördinaat
- * @param {number} y y-coördinaat
+ * Tekent schaap
  */
-var tekenKogel = function(x, y) {
+var tekenSchaap = function() {
+    var schaapY = [590];
+    var schaapX = [1140];
+    for (var i = 0; i < schaapX.length; i++) {
+    if(schaapIsZichtbaar === true) {
+      image(schaapImg, schaapX[i], schaapY[i], schaapBreedte, schaapHoogte) }
+    
 
+    if (collideRectRect(spelerX, spelerY, spelerBreedte, spelerHoogte, schaapX[i], schaapY[i], schaapBreedte, schaapHoogte)) {
+        schaapIsZichtbaar = false;
+        console.log("Schaap is niet meer zichtbaar");
+    }
+    }
+  
 
 };
 
+var test = 1;
 /**
  * Tekent de speler
  * @param {number} x x-coördinaat
  * @param {number} y y-coördinaat
  */
 var tekenSpeler = function(x, y) {
-  fill("white");
-  rect(x, y, spelerBreedte, spelerHoogte);
+   if (naarRechts === true || inDeLucht === true && springRichting > 0){ //speler is in de lucht en springt naar rechts
+     image(spelerImage[1], x, y, spelerBreedte, spelerHoogte);
+  } else if(naarLinks === true || inDeLucht === true && springRichting < 0) {
+      image(spelerImage[2], x, y, spelerBreedte, spelerHoogte);
+  } else { image(spelerImage[0], x, y, spelerBreedte, spelerHoogte); //speler beweegt niet naar links of rechts
+  }
 };
 
 
@@ -125,7 +150,33 @@ var beweegKogel = function() {
 
 };
 
+/**
+ * preload
+ * 
+ */
+function preload() {
+  spelerImage = [
+    loadImage('afbeeldingen/herder_staand.png'),
+    loadImage('afbeeldingen/herder1.png'),
+    loadImage('afbeeldingen/herderlinks1.png') ] ;
+    //loadImage('afbeeldingen/herder2.png'),
+    //loadImage('afbeeldingen/herder3.png'),
+    //loadImage('afbeeldingen/herder4.png')       
+  ondergrondImg = loadImage('afbeeldingen/ondergrond.png');
+  
+  obstakelImage = [ 
+      loadImage('afbeeldingen/water.png'),
+      loadImage('afbeeldingen/grasObstakel1.png'),
+      loadImage('afbeeldingen/grasObstakel2.png'),
+      loadImage('afbeeldingen/hout1.png')
+  ];
 
+  achtergrondImg = loadImage('afbeeldingen/achtergrond.png');
+  schaapImg = loadImage('afbeeldingen/schaap.png');
+
+}
+
+//obstakels
 var opValObstakel = false;
 var tegenObstakelRechts = false;
 var tegenObstakelLinks = false;
@@ -135,32 +186,36 @@ var staatOpObstakel = false;
 
 //duwObstakel 
 var duwObstakel = {
-  yPositie: 170,
+  yPositie: 140,
   breedte: 10,
-  hoogte: 90, }
+  hoogte: 140, }
 var duwObstakelX = 950;
 
 //valObstakel
 var valObstakel = {
-  xPositie: 630,
-  breedte: 320,
-  hoogte: 30,
+  xPositie: 660,
+  breedte: 290,
+  hoogte: 15,
 }
-var valObstakelY = 100;
+var valObstakelY = 130;
 
 
 
 var obstakel = function() {
-  var obstakelX =       [260, 525, 810, 950, 450, 105, 20 , 190, 950] 
-  var obstakelY=        [570, 475, 470, 260, 260, 345, 230, 100, 20 ]
-  var obstakelBreedte = [100, 100, 430, 150, 180, 180, 80 , 440, 10 ] 
-  var obstakelHoogte =  [80 , 175, 60 , 210, 30 , 30 , 30 , 30 , 150] 
+  var obstakelX =       [260, 525, 810, 950, 410, 150, 20 , 190, 950, 360] 
+  var obstakelY=        [590, 500, 470, 280, 280, 365, 250, 130, 20 , 610 ]
+  var obstakelBreedte = [100, 100, 430, 150, 250, 180, 80 , 470, 10 , 165 ] 
+  var obstakelHoogte =  [100 , 192, 60 , 210, 15 , 30 , 30 , 15 , 120, 90  ] 
 
-  
   //platforms
   for (var i = 0; i < obstakelX.length; i++) {
     fill("red");
     rect(obstakelX[i], obstakelY[i], obstakelBreedte[i], obstakelHoogte[i]); // de obstakels
+
+    image(obstakelImage[0], obstakelX[9], obstakelY[9], obstakelBreedte[9], obstakelHoogte[9]);
+    image(obstakelImage[1], obstakelX[0], obstakelY[0], obstakelBreedte[0], obstakelHoogte[0]);
+    image(obstakelImage[2], obstakelX[1], obstakelY[1], obstakelBreedte[1], obstakelHoogte[1]);
+    image(obstakelImage[3], obstakelX[2], obstakelY[2], obstakelBreedte[2], obstakelHoogte[2]);
 
     if (collideRectRect(spelerX, spelerY, spelerBreedte, spelerHoogte, obstakelX[i], obstakelY[i], obstakelBreedte[i], 1)) {
       spelerY = obstakelY[i] - spelerHoogte; //speler staat op obstakel
@@ -201,8 +256,8 @@ var obstakel = function() {
         versnelling = valSnelheid; //speler valt naar beneden ('botst' tegen onderkant obstakel)
      
      }
+    
   }
-
   /*
   //modder - als speler hierop staat kan hij minder hoog springen
   if (spelerX + spelerBreedte >= obstakelX[2] + 60 && spelerX + spelerBreedte <= obstakelX[2] + obstakelBreedte[2] && spelerY + spelerHoogte >= obstakelY[2] - 200 && 
@@ -263,6 +318,8 @@ var obstakel = function() {
 
 var beweegSpeler = function() {
   if (keyIsPressed && keyCode === 65 && spelerX > 20 && tegenObstakelRechts === false) { // links "a"
+    naarRechts = false;
+    naarLinks = true;
     nieuweSnelheid = spelerSnelheid + spelerVersnelling; // de snelheid van de speler neemt toe 
     spelerSnelheid = nieuweSnelheid;
     spelerX = spelerX - nieuweSnelheid
@@ -271,6 +328,8 @@ var beweegSpeler = function() {
      }
   
   else if (keyIsPressed && keyCode === 68 && spelerX + spelerBreedte <= veldBreedte + 20 && tegenObstakelLinks === false) { // rechts "d"
+    naarLinks = false;
+    naarRechts = true;
     nieuweSnelheid = spelerSnelheid + spelerVersnelling; // de snelheid van de speler neemt toe 
     spelerSnelheid = nieuweSnelheid;
     spelerX = spelerX + nieuweSnelheid;
@@ -281,6 +340,8 @@ var beweegSpeler = function() {
   else {
     spelerSnelheid = 0.25; // de snelheid van de speler wordt weer normaal als de speler heeft stilgestaan
     nieuweSnelheid = 0;
+    naarLinks = false;
+    naarRechts = false;
   } 
 
   if (spelerSnelheid >= maxSnelheid) { //de speler kan niet sneller dan de max snelheid
@@ -289,16 +350,19 @@ var beweegSpeler = function() {
 };
 
 //LATER WEGHALEN
-/* function locatie() { //om te kijken op welke plaats je de obstakels wilt plaatsen
-  fill("white");
-  text("X: "+mouseX, 800, 300);
-  text("Y: "+mouseY, 800, 350);
-} */
+//function locatie() { //om te kijken op welke plaats je de obstakels wilt plaatsen
+  //fill("white");
+  //text("X: "+mouseX, 800, 300);
+  //text("Y: "+mouseY, 800, 350);
+//} 
+
+var inDeLucht = false;
 
 function spelerSpringen() {
   if (keyIsPressed && keyCode === 32 && moetVallen === false) { // spatie
     jump = true; //springen
     staatOpObstakel = false;
+    inDeLucht = true; //speler is in de lucht 
   }
   
   else {
@@ -345,6 +409,7 @@ function zwaartekracht() {
     sprongTeller = 0; //reset sprongTeller wanneer speler landt
     tegenObstakelRechts = false; //de speler kan bewegen en staat dus niet stil tegen de zijkant van een obstakel
     tegenObstakelLinks = false;
+    inDeLucht = false; //speler is niet meer in de lucht
   } else  { 
     spelerY = spelerY + (richting*versnelling);
     spelerX = spelerX + springRichting; 
@@ -385,7 +450,7 @@ function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(canvasBreedte, canvasHoogte);
   background('black');
-  alert("(Voor Lisanne) A = links, D = rechts, spatie = springen");
+  alert("(Voor Lisanne) A = links, D = rechts, spatie = springen\n De rode obstakels worden weggehaald als alles een plaatje heeft");
 }
 
 
@@ -396,6 +461,10 @@ function setup() {
  */
 function draw() {
   switch (spelStatus) {
+    case GAMEOVER:
+        alert("GAMEOVER");
+    break; 
+    
     case UITLEG:
       background(0);
       fill(225,225,225);
@@ -423,7 +492,7 @@ function draw() {
       zwaartekracht();
       tekenVeld();
       tekenVijand(vijandX, vijandY);
-      tekenKogel(kogelX, kogelY);
+      tekenSchaap();
       tekenSpeler(spelerX, spelerY);
       obstakel();
       //locatie();
